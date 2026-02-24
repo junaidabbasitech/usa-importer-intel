2bb67a4dc8e25c9054edfc0787af33fd5fb95d28
Webvista <webvista@@gmail.com>
2026-02-24 11:31:46 -0500

    rfv

---
+++ types.js
@@ -0,0 +1,9 @@
+// Step enum for tracking the current stage of the trade intelligence workflow.
+export var Step;
+(function (Step) {
+    Step["INPUT"] = "INPUT";
+    Step["ANALYZE"] = "ANALYZE";
+    Step["CONFIGURE"] = "CONFIGURE";
+    Step["EXECUTE"] = "EXECUTE";
+})(Step || (Step = {}));
+//# sourceMappingURL=types.js.map
